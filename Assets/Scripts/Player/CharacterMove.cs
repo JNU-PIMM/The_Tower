be1d03457250241090e3852b67794be82352ef63
DESKTOP-BFN2B16\USER <na31766@gmail.com>
2020-12-26 20:24:22 +0900

    feat : make event

--- Assets/Scripts/Player/CharacterMove.cs
+++ Assets/Scripts/Player/CharacterMove.cs
@@ -1,13 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 public class CharacterMove : MonoBehaviour
 
 {
+    public Action<int> collidedWithEnemyEvent;
+    public Action pressUpEvent;
+    public Action pressDownEvent;
+    public Action gameoverEvent;
     // Update is called once per frame
     public float height;
+    public int life;
     int speed = 10; //스피드
     float xMove, yMove;
     bool keyInput = true;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collidedWithEnemyEvent(life);
+
+            if (life > 0)
+                life = life - 1;
+            else
+                gameoverEvent();
+            //게임종료
+        }
+
+
+    }
+
     void Update()
     {
         xMove = 0;
@@ -20,9 +43,18 @@ public class CharacterMove : MonoBehaviour
             else if (Input.GetKey(KeyCode.LeftArrow))
                 xMove = -speed * Time.deltaTime;
             if (Input.GetKey(KeyCode.UpArrow))
-                height = speed * Time.deltaTime;
+            {
+                height += speed * Time.deltaTime;
+                pressUpEvent();
+            }
+
             else if (Input.GetKey(KeyCode.DownArrow))
-                height = -speed * Time.deltaTime;
+            {
+                height += -speed * Time.deltaTime;
+                pressDownEvent();
+                    
+            }
+ 
             this.transform.Translate(new Vector3(xMove, yMove, 0));
         }
         if(Input.GetKeyDown(KeyCode.Space))
