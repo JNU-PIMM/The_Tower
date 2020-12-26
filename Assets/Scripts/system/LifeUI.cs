081d32521efdfdc29867c687e8d3fe131c684e03
unknown <201378@jnu.ac.kr>
2020-12-26 15:49:17 +0900

    feat : lifeUI 추가

---
+++ Assets/Scripts/system/LifeUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeUI : MonoBehaviour
+{
+    public List<GameObject> lifes;
+
+    // Start is called before the first frame update
+    void Start()
+    {    
+        SetLifeimg(2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetLifeimg(int life)
+    {
+
+       for(int i = 0; i < lifes.Count; i++)
+        {
+            if (i < life)
+                lifes[i].SetActive(true);
+            else 
+                lifes[i].SetActive(false);
+
+        }
+    }
+
+}
