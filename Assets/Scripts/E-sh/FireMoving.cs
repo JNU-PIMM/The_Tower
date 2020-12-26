a583d3c212e7592bd0bf6c1c8b92f68789669197
E-SeongHo <lsh031255@naver.com>
2020-12-26 16:48:59 +0900

    feat : fire

---
+++ Assets/Scripts/E-sh/FireMoving.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireMoving : MonoBehaviour
+{
+    [SerializeField] float speed = 0.1f;
+
+    void Update()
+    {
+        gameObject.transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+}
